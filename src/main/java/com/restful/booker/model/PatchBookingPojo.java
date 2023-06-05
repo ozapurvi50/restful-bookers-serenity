2d915d757eee294b94385e95c859807f43651e3c
Purvi Oza <ozapurvi50@gmail.com>
2023-06-05 01:29:09 +0100

    Restful-Bookers-Serenity

---
+++ src/main/java/com/restful/booker/model/PatchBookingPojo.java
@@ -0,0 +1,16 @@
+package com.restful.booker.model;
+
+public class PatchBookingPojo {
+
+    private String additionalneeds;
+
+    public String getAdditionalneeds() {
+
+        return additionalneeds;
+    }
+
+    public void setAdditionalneeds(String additionalneeds) {
+
+        this.additionalneeds = additionalneeds;
+    }
+}
